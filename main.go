a1cbadb06394333da702ee34c0140799c735968a
MarkusFelsner <mail@markus-felsner.de>
2023-07-03 13:18:36 +0200

    bereinigt

--- main.go
+++ main.go
@@ -1,17 +1,66 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
-}
+const (
+	dbHost     = "sdvdemo-mysql"
+	dbPort     = 3306
+	dbUser     = "wordpress"
+	dbPassword = "Sommer123456782!"
+	dbName     = "wordpress"
+)
 
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Running demo app. Press Ctrl+C to exit...")
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	// Verbinde mit der MySQL-Datenbank
+	dbInfo := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", dbInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Erstelle den HTTP-Handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM your_table")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var id int
+		var name string
+		var age int
+
+		var result string
+
+		for rows.Next() {
+			err := rows.Scan(&id, &name, &age)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			result += fmt.Sprintf("ID: %d, Name: %s, Age: %d\n", id, name, age)
+		}
+
+		if err := rows.Err(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprint(w, result)
+	})
+
+	// Starte den HTTP-Server
+	log.Println("Server gestartet, höre auf Port 8080...")
+	if err := http.ListenAndServe(":8888", nil); err != nil {
+		log.Fatal(err)
+	}
 }
